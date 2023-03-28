0d465b98cb9d0acd600155f5a76880569a1c7c35
AdrienLgl <adrien.laigle35@gmail.com>
2023-03-28 09:28:45 +0200

    project init

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/adc.c \
+../Src/gpio.c \
+../Src/main.c \
+../Src/stm32l0xx_it.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32l0xx.c \
+../Src/usart.c 
+
+OBJS += \
+./Src/adc.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/stm32l0xx_it.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32l0xx.o \
+./Src/usart.o 
+
+C_DEPS += \
+./Src/adc.d \
+./Src/gpio.d \
+./Src/main.d \
+./Src/stm32l0xx_it.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32l0xx.d \
+./Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DSTM32L053xx -DUSE_FULL_LL_DRIVER -DHSE_VALUE=8000000 -DHSE_STARTUP_TIMEOUT=100 -DLSE_STARTUP_TIMEOUT=5000 -DLSE_VALUE=32768 -DMSI_VALUE=2097000 -DHSI_VALUE=16000000 -DLSI_VALUE=37000 -DVDD_VALUE=3300 -DPREFETCH_ENABLE=0 -DINSTRUCTION_CACHE_ENABLE=1 -DDATA_CACHE_ENABLE=1 -c -I../Inc -I../Drivers/CMSIS/Include -I../Drivers/STM32L0xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32L0xx/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/adc.d ./Src/adc.o ./Src/adc.su ./Src/gpio.d ./Src/gpio.o ./Src/gpio.su ./Src/main.d ./Src/main.o ./Src/main.su ./Src/stm32l0xx_it.d ./Src/stm32l0xx_it.o ./Src/stm32l0xx_it.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32l0xx.d ./Src/system_stm32l0xx.o ./Src/system_stm32l0xx.su ./Src/usart.d ./Src/usart.o ./Src/usart.su
+
+.PHONY: clean-Src
+
